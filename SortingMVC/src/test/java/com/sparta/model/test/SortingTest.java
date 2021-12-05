7b32aae3683e5acda6e39eb9a4644ef54baa6041
grimz5129 <59632166+grimz5129@users.noreply.github.com>
2021-12-05 14:39:33 +0000

    Added Tree sort tests
    added a set of tests for the tree sort

--- SortingMVC/src/test/java/com/sparta/model/test/SortingTest.java
+++ SortingMVC/src/test/java/com/sparta/model/test/SortingTest.java
@@ -1,7 +1,5 @@
 package com.sparta.model.test;
 
-import com.sparta.mvc.model.BubbleSort;
-import com.sparta.mvc.model.QuickSort;
 import com.sparta.mvc.model.SorterFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,12 +13,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SortingTest {
 
+    /**
+     * Bubble Sort Tests
+     */
     @DisplayName("Testing bubble sort")
     @Test
     public void testBubbleSort(){
         String arrExp = "[0, 2, 11, 32, 51, 72, 179, 234]";
         int arr[] = {179,51,72,32,234,11,2,0};
-//        assertEquals(arrExp, Arrays.toString(new BubbleSort().sort(arr)));
         assertEquals(arrExp, Arrays.toString(SorterFactory.getType("bubble").sort(arr)));
     }
 
@@ -29,15 +29,18 @@ public class SortingTest {
     public void testBubbleSortWithNegative(){
         String arrExp = "[-2, 0, 11, 32, 51, 72, 179, 234]";
         int arr[] = {179,51,72,32,234,11,-2,0,};
-        assertEquals(arrExp, Arrays.toString(new BubbleSort().sort(arr)));
+        assertEquals(arrExp, Arrays.toString(SorterFactory.getType("bubble").sort(arr)));
     }
 
+    /**
+     * Quick Sort Tests
+     */
     @DisplayName("Testing quick sort")
     @Test
     public void testQuickSort(){
         String arrExp = "[0, 2, 11, 32, 51, 72, 179, 234]";
         int arr[] = {179,51,72,32,234,11,2,0};
-        assertEquals(arrExp, Arrays.toString(new QuickSort().sort(arr)));
+        assertEquals(arrExp, Arrays.toString(SorterFactory.getType("quick").sort(arr)));
     }
 
     @DisplayName("Testing quick sort with negative number")
@@ -45,10 +48,31 @@ public class SortingTest {
     public void testQuickSortWithNegative(){
         String arrExp = "[-2, 0, 11, 32, 51, 72, 179, 234]";
         int arr[] = {179,51,72,32,234,11,-2,0};
-        assertEquals(arrExp, Arrays.toString(new BubbleSort().sort(arr)));
+        assertEquals(arrExp, Arrays.toString(SorterFactory.getType("quick").sort(arr)));
     }
 
-    @DisplayName("Testing collection sort with list")
+    /**
+     * Collection/Arrays Sort Tests
+     */
+    @DisplayName("Testing collection sort with arrays")
+    @Test
+    public void testArraysSort(){
+        int arr[] = {179,51,72,32,234,11,-2,0};
+        String arrExp = "[-2, 0, 11, 32, 51, 72, 179, 234]";
+        Arrays.sort(arr);
+        assertEquals(arrExp, Arrays.toString(arr));
+    }
+
+    @DisplayName("Testing collection sort with arrays negatives")
+    @Test
+    public void testArraysSortNegative(){
+        int arr[] = {179,51,72,32,234,11,-2,0};
+        String arrExp = "[-2, 0, 11, 32, 51, 72, 179, 234]";
+        Arrays.sort(arr);
+        assertEquals(arrExp, Arrays.toString(arr));
+    }
+
+    @DisplayName("Testing collection sort")
     @Test
     public void testCollectionSort(){
         List<Integer> myArr = new ArrayList<>(List.of(2,4,56,3,242,5,23,424,1,423,4,234,25,235));
@@ -57,5 +81,33 @@ public class SortingTest {
         assertEquals(arrExp, myArr.toString());
     }
 
+    @DisplayName("Testing collection sort with negatives")
+    @Test
+    public void testCollectionSortNegatives(){
+        List<Integer> myArr = new ArrayList<>(List.of(-2,-4,-56,-3,-242,-5,-23,-424,1,-423,-4,-234,25,-235));
+        String arrExp = "[-424, -423, -242, -235, -234, -56, -23, -5, -4, -4, -3, -2, 1, 25]";
+        Collections.sort(myArr);
+        assertEquals(arrExp, myArr.toString());
+    }
+
+
+    /**
+     * Tree Sort Tests
+     */
+    @DisplayName("Testing bubble sort")
+    @Test
+    public void testTreeSort(){
+        String arrExp = "[0, 22, 24, 62, 121, 194, 200, 531]";
+        int arr[] = {194,531,22,62,24,121,200,0};
+        assertEquals(arrExp, Arrays.toString(SorterFactory.getType("bubble").sort(arr)));
+    }
+
+    @DisplayName("Testing bubble sort with negative number")
+    @Test
+    public void testTreeSortWithNegative(){
+        String arrExp = "[-531, -200, -194, -62, -24, 0, 22, 121]";
+        int arr[] = {-194,-531,22,-62,-24,121,-200,0};
+        assertEquals(arrExp, Arrays.toString(SorterFactory.getType("bubble").sort(arr)));
+    }
 
 }
